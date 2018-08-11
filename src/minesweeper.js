dd028c0a73a57a30bf1e8f34c2af046c5ec8cd5e
unknown <fortin_85@hotmail.com>
2018-08-11 17:58:10 -0400

    not that I have a board dynamically being created hardcoded a user guess to simulate what the board would look like

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,3 +1,4 @@
+//after creating the board element created a constant for print board, it will print off the three nested elements and join a | with spaces to act as a game board
 const printBoard = board => {
   console.log('Current Board:');
   console.log(board[0].join(' | '));
@@ -5,14 +6,18 @@ const printBoard = board => {
   console.log(board[2].join(' | '));
 };
 
+//create a variable called board and created three nested arrays to act like a game board
 let board = [
   [' ',' ',' '],
   [' ',' ',' '],
   [' ',' ',' ']
 ];
 
+//call function and passed off the board array as an argument
+printBoard(board);
 
+//hard coding a users guess and bomb in place
+board[0][1] = 1;
+board[2][2] = 'B';
 
-console.log(board[0][0]);
-
-//printBoard();
+printBoard(board);
