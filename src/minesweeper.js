cc316ce85910cbe7a8da0a36e46bcf640d24f7b4
unknown <fortin_85@hotmail.com>
2018-08-11 17:53:33 -0400

    created print board function, display the three arrays and join a pipe in between the spaces

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,7 +1,18 @@
+const printBoard = board => {
+  console.log('Current Board:');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
+
 let board = [
   [' ',' ',' '],
   [' ',' ',' '],
   [' ',' ',' ']
 ];
 
-console.log(board);
+
+
+console.log(board[0][0]);
+
+//printBoard();
